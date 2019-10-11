d196ff07adee3124ddbc8a64fd66931c1efb37c4
James Sudbury <james.sudbury@btinternet.com>
2019-10-11 23:26:04 +0100

    Changed the name of the invoke cmdlet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,5 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.0.0] Unreleased
+## [1.0.0]
 
+- Added initial functionality
+  - Get-UIRoot
+  - Get-UIWindow
+  - Get-UIControl
+  - Invoke-UIControl
+  - Get-UIText
+  - Set-UIText
--- UIAutomation/Public/Invoke-UIControl.ps1
+++ UIAutomation/Public/Invoke-UIControl.ps1
@@ -1,6 +1,17 @@
-﻿function Invoke-UIAutomationElement {
+﻿<#
+.SYNOPSIS
+Invokes a UI control.
+.DESCRIPTION
+Performs an invoke action (i.e.:button click) on a control.
+.EXAMPLE
+PS> Get-UIWindow -Name "Untitled - Notepad" | Get-UIControl -Name "Close" | Invoke-UIControl
+
+Sets the text of the Notepad editor section to "Hello World!"
+#>
+function Invoke-UIControl {
     [CmdLetBinding()]
     param(
+        # The AutomationElement control to be invoked.
         [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateScript({
             try {
