64780bd141cd341fc47e0490d987799bc218584a
James Sudbury <james.sudbury@btinternet.com>
2019-10-14 21:38:56 +0100

    create the output file space.

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -3,8 +3,9 @@ properties {
     # This modifies the default behavior from the "Build" task
     # in the PowerShellBuild shared psake task module
     $PSBPreference.Build.CompileModule = $false
+    $PSBPreference.Test.OutputFile = "../out/testResults.xml"
 }
 
 task default -depends Test
 
-task Test -FromModule PowerShellBuild -Version '0.3.0'
+task Test -FromModule PowerShellBuild -Version '0.4.0'
